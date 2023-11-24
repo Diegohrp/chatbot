56e106683307e5611e020eb9ab6e449b4fbfe619
Xalot <jose.berrocal.97@hotmail.com>
2023-11-24 11:35:17 -0600

    Cambio para mensaje de entrada

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const { startBot } = require('./chat/index');
 
-// Cambio aquí: Usa el puerto proporcionado por la variable de entorno PORT de GCP o 3000 si no se proporciona
+// Cambia el puerto para que utilice la variable de entorno PORT de GCP o 3000 si se ejecuta localmente
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/chat'));
+// Sirve archivos estáticos de la carpeta 'public', si decides usarla en el futuro
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/chatbot', (req, res, next) => {
+// Ruta raíz que simplemente envía un mensaje de bienvenida
+app.get('/', (req, res) => {
+  res.send('Bienvenido al Chatbot del IMSS. Para interactuar usa la ruta /chatbot');
+});
+
+// Ruta para '/chatbot' que inicia la interacción con el bot
+app.get('/chatbot', (req, res) => {
   startBot(res);
 });
 
+// Configuración para que la aplicación escuche en el puerto correcto
 app.listen(port, () => {
-  console.log('Listening on port: ' + port);
+  console.log(`Listening on port: ${port}`);
 });
